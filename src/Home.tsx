de919b9714ddf14130fe90d310917269a7593c54
lei <oizhaolei@gmail.com>
2022-09-28 10:16:39 +0900

    vite, antd-mobile,less

---
+++ src/Home.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Space, Button } from "antd-mobile";
+import { DemoBlock } from "./demos";
+
+import reactLogo from "./assets/react.svg";
+import "./Home.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <DemoBlock title="填充模式">
+        <Space wrap>
+          <Button color="primary" fill="solid">
+            Solid
+          </Button>
+          <Button color="primary" fill="outline">
+            Outline
+          </Button>
+          <Button color="primary" fill="none">
+            None
+          </Button>
+        </Space>
+      </DemoBlock>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
